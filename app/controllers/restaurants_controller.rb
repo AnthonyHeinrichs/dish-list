1bf6b883da9cedab3d6f509d0322e01cc6df668a
Dan Deift <dan.deift@aol.com>
2022-03-07 12:37:50 +0100

    added update to the restaurant controller a update and edit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,6 +11,16 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def  update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_path)
+    redirect_to restaurant_path(@restaurnat)
+  end
+
   def index
     @restaurants = Restaurant.all
 
