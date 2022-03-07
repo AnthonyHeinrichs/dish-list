ea73157e59412bf556850ce028c03ae45743b00e
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-03-07 14:41:30 +0100

    added autocomplete off for forms

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     raise
+    # Need to add logic so the review saves for a specific restaurant's dish.
     @review.save
     redirect_to restaurants_path
   end
