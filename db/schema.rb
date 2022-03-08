237baa692223436527e5e8e02294a3938764a0b1
Dan Deift <dan.deift@aol.com>
2022-03-08 12:49:26 +0100

    added column telephone to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_100636) do
+ActiveRecord::Schema.define(version: 2022_03_08_114825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_03_08_100636) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "telephone"
   end
 
   create_table "reviews", force: :cascade do |t|
