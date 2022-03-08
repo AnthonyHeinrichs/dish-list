b4c5c44ad629924e83990ef4b17ab7a7d262fd6c
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-03-08 21:11:43 +0100

    add new seeds, added auto generated text to restaurant modal

---
+++ db/migrate/20220308194918_add_description_to_restaurant.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToRestaurant < ActiveRecord::Migration[6.1]
+  def change
+    add_column :restaurants, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_190556) do
+ActiveRecord::Schema.define(version: 2022_03_08_194918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_03_08_190556) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
   end
 
   create_table "reviews", force: :cascade do |t|
