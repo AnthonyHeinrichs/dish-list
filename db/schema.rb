f905ecd47b29b76d63b63fb232c89307e8515ed8
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-03-09 12:50:27 +0100

    Changed card colors & shadow

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_03_08_194918) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,9 +97,9 @@ ActiveRecord::Schema.define(version: 2022_03_08_194918) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
     t.string "telephone"
     t.string "website"
-    t.text "description"
   end
 
   create_table "reviews", force: :cascade do |t|
