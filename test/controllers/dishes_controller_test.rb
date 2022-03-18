ff7e4894075e8efceb66e807d6c6cd9333fcee9c
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-03-18 20:04:30 +0100

    added dish controller test for adding favorite

--- test/controllers/dishes_controller_test.rb
+++ test/controllers/dishes_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class DishesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'That favorite dishes can be saved' do
+  dish = Dish.last
+  assert FavoriteDish.create(user: User.last, dish: dish)
+  end
 end
--- test/models/dish_test.rb
+++ test/models/dish_test.rb
@@ -5,4 +5,9 @@ class DishTest < ActiveSupport::TestCase
     dish = Dish.all
     assert_not dish.nil?
   end
+
+  test 'that dishes have names' do
+    dish = Dish.last
+    assert_not dish.present?
+  end
 end
