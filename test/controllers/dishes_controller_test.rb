896e749cd2b66a93bf66089bc8184e9150f4d679
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-03-21 17:26:16 +0100

    New test to check if new dishes can be created

--- test/controllers/dishes_controller_test.rb
+++ test/controllers/dishes_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class DishesControllerTest < ActionDispatch::IntegrationTest
-  test 'That favorite dishes can be saved' do
-  dish = Dish.last
-  assert FavoriteDish.create(user: User.last, dish: dish)
+  test 'That favorite dishes can be saved to favorites list' do
+    dish = Dish.last
+    assert FavoriteDish.create(user: User.last, dish: dish)
+  end
+
+  test 'That new dishes can be created' do
+    dish = Dish.new(name: 'test', country: 'CA', spiciness: 3, description: 'test 123456!@#')
+    assert dish.valid?
   end
 end
